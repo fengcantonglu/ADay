b0043bd88186c68725f6d1605f51d52e16f44fb4
fengcantonglu <937769884@qq.com>
2016-04-07 08:57:22 +0800

    视频的页面

---
+++ src/com/can/aday/VideoClassifyDetailsActivity.java
@@ -0,0 +1,73 @@
+package com.can.aday;
+
+import java.util.ArrayList;
+
+import com.can.aday.adapter.VideoClassifyDetailsBaseAdapter;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class VideoClassifyDetailsActivity extends Activity {
+	TextView titleName;//标题
+	ImageView backImage;//返回图标
+	ListView listview;
+	VideoClassifyDetailsBaseAdapter vCDBaseAdapter;
+	ArrayList<String> list;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.video_classify_details_main);
+		findView();
+		getData();
+		vCDBaseAdapter=new VideoClassifyDetailsBaseAdapter(list,this);
+		listview.setAdapter(vCDBaseAdapter);
+	}
+	/**
+	 * 获取数据
+	 */
+	private void getData() {
+		String str[]={"#冲浪","#冲浪","#冲浪","#冲浪","#冲浪","#冲浪"};
+		list=new ArrayList<String>();
+		for(int i=0;i<6;i++){
+			list.add(str[i]);
+		}
+	}
+	/**
+	 * 控件ID
+	 */
+	private void findView() {
+		titleName=(TextView)findViewById(R.id.title_bar_title_text);
+		listview=(ListView)findViewById(R.id.video_classify_details_listview);
+		backImage=(ImageView)findViewById(R.id.title_bar_back_icon);
+		
+		titleName.setText("运动");
+		listview.setSelector(new ColorDrawable(Color.TRANSPARENT));
+		
+		backImage.setOnClickListener(clickListener);
+	}
+	/**
+	 * 简单的点击事件
+	 */
+	OnClickListener clickListener=new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.title_bar_back_icon:
+				finish();
+				break;
+
+			default:
+				break;
+			}
+		}
+	};
+}
